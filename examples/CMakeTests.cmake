9ea72ca1bcfda311f614bcb868ccb40ebd17a24d
Jordan Henderson <jhenderson@hdfgroup.org>
2018-02-07 14:00:08 -0600

    Add in missing examples folder from a previous commit
    Add in autotools conditional for tests
    
    Updates to CMake

---
+++ examples/CMakeTests.cmake
@@ -0,0 +1,128 @@
+
+##############################################################################
+##############################################################################
+###           T E S T I N G                                                ###
+##############################################################################
+##############################################################################
+  file (MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/red ${PROJECT_BINARY_DIR}/blue ${PROJECT_BINARY_DIR}/u2w)
+  if (BUILD_SHARED_LIBS)
+    file (MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/RVEX-shared")
+    file (MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/RVEX-shared/red ${PROJECT_BINARY_DIR}/RVEX-shared/blue ${PROJECT_BINARY_DIR}/RVEX-shared/u2w)
+  endif (BUILD_SHARED_LIBS)
+
+  # Remove any output file left over from previous test run
+  add_test (
+      NAME EXAMPLES-clear-objects
+      COMMAND    ${CMAKE_COMMAND}
+          -E remove 
+          Attributes.h5
+          btrees_file.h5
+          cmprss.h5
+          default_file.h5
+          dset.h5
+          extend.h5 
+          extlink_prefix_source.h5
+          extlink_source.h5
+          extlink_target.h5
+          group.h5
+          groups.h5
+          hard_link.h5
+          mount1.h5
+          mount2.h5
+          one_index_file.h5
+          only_dspaces_and_attrs_file.h5
+          only_huge_mesgs_file.h5
+          REF_REG.h5
+          refere.h5
+          SDS.h5
+          SDScompound.h5
+          SDSextendible.h5
+          Select.h5
+          separate_indexes_file.h5
+          small_lists_file.h5
+          soft_link.h5
+          subset.h5
+          unix2win.h5
+          blue/prefix_target.h5
+          red/prefix_target.h5
+          u2w/u2w_target.h5
+          vds.h5
+          vds-excalibur.h5
+          vds-exclim.h5
+          vds-percival.h5
+          vds-percival-unlim.h5
+          vds-percival-unlim-maxmin.h5
+          a.h5
+          b.h5
+          c.h5
+          d.h5
+          vds-simpleIO.h5
+          vds-eiger.h5
+  )
+  if (NOT "${last_test}" STREQUAL "")
+    set_tests_properties (EXAMPLES-clear-objects PROPERTIES DEPENDS ${last_test})
+  endif (NOT "${last_test}" STREQUAL "")
+  set (last_test "EXAMPLES-clear-objects")
+
+  foreach (example ${examples})
+    add_test (NAME EXAMPLES-${example} COMMAND $<TARGET_FILE:${example}>)
+    if (NOT "${last_test}" STREQUAL "")
+      set_tests_properties (EXAMPLES-${example} PROPERTIES DEPENDS ${last_test})
+    endif (NOT "${last_test}" STREQUAL "")
+    set (last_test "EXAMPLES-${example}")
+  endforeach (example ${examples})
+
+  if (BUILD_SHARED_LIBS)
+    # Remove any output file left over from previous test run
+    add_test (
+        NAME EXAMPLES-shared-clear-objects
+        COMMAND    ${CMAKE_COMMAND}
+            -E remove 
+            Attributes.h5
+            btrees_file.h5
+            cmprss.h5
+            default_file.h5
+            dset.h5
+            extend.h5 
+            extlink_prefix_source.h5
+            extlink_source.h5
+            extlink_target.h5
+            group.h5
+            groups.h5
+            hard_link.h5
+            mount1.h5
+            mount2.h5
+            one_index_file.h5
+            only_dspaces_and_attrs_file.h5
+            only_huge_mesgs_file.h5
+            REF_REG.h5
+            refere.h5
+            SDS.h5
+            SDScompound.h5
+            SDSextendible.h5
+            Select.h5
+            separate_indexes_file.h5
+            small_lists_file.h5
+            soft_link.h5
+            subset.h5
+            unix2win.h5
+            blue/prefix_target.h5
+            red/prefix_target.h5
+            u2w/u2w_target.h5
+        WORKING_DIRECTORY
+            ${PROJECT_BINARY_DIR}/H5EX-shared
+    )
+    if (NOT "${last_test}" STREQUAL "")
+      set_tests_properties (EXAMPLES-shared-clear-objects PROPERTIES DEPENDS ${last_test})
+    endif (NOT "${last_test}" STREQUAL "")
+    set (last_test "EXAMPLES-shared-clear-objects")
+
+    foreach (example ${examples})
+      add_test (NAME EXAMPLES-shared-${example} COMMAND $<TARGET_FILE:${example}-shared>)
+      set_tests_properties (EXAMPLES-shared-${example} PROPERTIES WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/RVEX-shared)
+      if (NOT "${last_test}" STREQUAL "")
+        set_tests_properties (EXAMPLES-shared-${example} PROPERTIES DEPENDS ${last_test})
+      endif (NOT "${last_test}" STREQUAL "")
+      set (last_test "EXAMPLES-shared-${example}")
+    endforeach (example ${examples})
+  endif (BUILD_SHARED_LIBS)
---
+++ examples/h5_crtatt.c
@@ -0,0 +1,79 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/*
+ *  This example illustrates how to create an attribute attached to a
+ *  dataset.  It is used in the HDF5 Tutorial.
+ */
+
+#include "hdf5.h"
+#define FILE "dset.h5"
+#define FILE_NAME_MAX_LENGTH 256
+
+int main() {
+
+   hid_t       file_id, fapl_id, dataset_id, attribute_id, dataspace_id;  /* identifiers */
+   hsize_t     dims;
+   int         attr_data[2];
+   const char *username;
+   char        filename[FILE_NAME_MAX_LENGTH];
+   herr_t      status;
+
+   RVinit();
+
+   /* Initialize the attribute data. */
+   attr_data[0] = 100;
+   attr_data[1] = 200;
+
+   fapl_id = H5Pcreate(H5P_FILE_ACCESS);
+   H5Pset_fapl_rest_vol(fapl_id);
+
+   username = getenv("HSDS_USERNAME");
+
+   snprintf(filename, FILE_NAME_MAX_LENGTH, "/home/%s/" FILE, username);
+
+   /* Open an existing file. */
+   file_id = H5Fopen(filename, H5F_ACC_RDWR, fapl_id);
+
+   /* Open an existing dataset. */
+   dataset_id = H5Dopen2(file_id, "/dset", H5P_DEFAULT);
+
+   /* Create the data space for the attribute. */
+   dims = 2;
+   dataspace_id = H5Screate_simple(1, &dims, NULL);
+
+   /* Create a dataset attribute. */
+   attribute_id = H5Acreate2 (dataset_id, "Units", H5T_STD_I32BE, dataspace_id, 
+                             H5P_DEFAULT, H5P_DEFAULT);
+
+   /* Write the attribute data. */
+   status = H5Awrite(attribute_id, H5T_NATIVE_INT, attr_data);
+
+   /* Close the attribute. */
+   status = H5Aclose(attribute_id);
+
+   /* Close the dataspace. */
+   status = H5Sclose(dataspace_id);
+
+   /* Close to the dataset. */
+   status = H5Dclose(dataset_id);
+
+   status = H5Pclose(fapl_id);
+
+   /* Close the file. */
+   status = H5Fclose(file_id);
+
+   RVterm();
+}
