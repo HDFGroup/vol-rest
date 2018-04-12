346cf32cca519c9784c08ebe60a31bc184c8e888
Jordan Henderson <jhenderson@hdfgroup.org>
2018-04-12 12:21:44 -0500

    Add empty beginning to REST VOL's CMake build batch file

---
+++ build_vol_cmake.bat
@@ -0,0 +1,16 @@
+rem Copyright by The HDF Group.                                              
+rem All rights reserved.                                                     
+rem
+rem This file is part of HDF5. The full HDF5 copyright notice, including     
+rem terms governing use, modification, and redistribution, is contained in   
+rem the files COPYING and Copyright.html.  COPYING can be found at the root  
+rem of the source code distribution tree; Copyright.html can be found at the 
+rem root level of an installed copy of the electronic document set and is    
+rem linked from the top-level documents page.  It can also be found at       
+rem http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have access  
+rem to either file, you may request a copy from help@hdfgroup.org.           
+rem
+rem A script used to first configure and build the HDF5 source distribution
+rem included with the REST VOL plugin source code, and then use that built
+rem HDF5 to build the REST VOL plugin itself.
+
