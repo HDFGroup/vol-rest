bf9874258db761860e402018cdf283951986385d
Jan-Willem Blokland <Jan-Willem.Blokland@Shell.com>
2023-08-04 11:21:37 +0200

    CMake: (feature+fix) HDF5 and CMake Config files
    - Implemented support for making using of find_package(HDF5).
    - Improved FindYAJL.cmake. Now one can select shared or static
      YAJL library version.
    - Improved the CMake build system such proper hdf5_vol_rest-config.cmake,
      hdf5_vol_rest-config-version.cmake and hdf5_vol_rest-targets-*.cmake
      are generated. Also install the improved FindYAJL.cmake. This is
      all needed to more easily make use of VOL REST API in other CMake
      projects.
    - Improved the README.md file.
    - Improved the build_vol_cmake.sh file.

--- CMake/FindYAJL.cmake
+++ CMake/FindYAJL.cmake
@@ -1,28 +1,51 @@
 # - Try to find libyajl
-# Once done this will define
-#  YAJL_FOUND - System has libyajl
-#  YAJL_INCLUDE_DIRS - The libyajl include directories
-#  YAJL_LIBRARIES - The libraries needed to use libyajl
-#  YAJL_DEFINITIONS - Compiler switches required for using libyajl
+#
+# The variable YAJL_USE_STATIC_LIBRARIES can be used to select
+# the static YAJL library.
+#
+# Once done this will define the variables
+#   YAJL_FOUND        - System has libyajl
+#   YAJL_INCLUDE_DIR  - The libyajl include directory
+#   YAJL_INCLUDE_DIRS - The libyajl include directories
+#   YAJL_LIBRARIES    - The libraries needed to use libyajl
+# and the targets
+#   yajl              - shared or static YAJL library depending on the value of YAJL_USE_STATIC_LIBRARIES variable.
+#   yajl-shared       - Dedicated shared YAJL library target
+#   yajl-static       - Dedicated static YAJL library target
 
-find_package(PkgConfig)
-pkg_check_modules(PC_YAJL QUIET yajl)
-set(YAJL_DEFINITIONS ${PC_YAJL_CFLAGS_OTHER})
-
-find_path(YAJL_INCLUDE_DIR yajl/yajl_version.h
-          HINTS ${PC_YAJL_INCLUDEDIR} ${PC_YAJL_INCLUDE_DIRS}
-          PATH_SUFFIXES libyajl )
-
-find_library(YAJL_LIBRARY NAMES yajl libyajl
-             HINTS ${PC_YAJL_LIBDIR} ${PC_YAJL_LIBRARY_DIRS} )
+find_path(YAJL_INCLUDE_DIR yajl/yajl_version.h DOC "YAJL include directory")
+find_library(YAJL_SHARED_LIBRARY NAMES yajl libyajl DOC "Shared YAJL library")
+find_library(YAJL_STATIC_LIBRARY NAMES yajl_s libyajl_s DOC "Static YAJL library")
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBYAJL_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(yajl DEFAULT_MSG
-                                  YAJL_LIBRARY YAJL_INCLUDE_DIR)
+find_package_handle_standard_args(YAJL DEFAULT_MSG
+                                  YAJL_SHARED_LIBRARY YAJL_STATIC_LIBRARY YAJL_INCLUDE_DIR)
 
-mark_as_advanced(YAJL_INCLUDE_DIR YAJL_LIBRARY )
+if (YAJL_FOUND)
+  add_library(yajl-shared SHARED IMPORTED)
+  add_library(yajl-static STATIC IMPORTED)
+  if (YAJL_USE_STATIC_LIBRARIES)
+    add_library(yajl ALIAS yajl-static)
+  else ()
+    add_library(yajl ALIAS yajl-shared)
+  endif ()
+  set_target_properties(yajl-shared PROPERTIES
+    IMPORTED_LOCATION "${YAJL_SHARED_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${YAJL_INCLUDE_DIR}"
+  )
+  set_target_properties(yajl-static PROPERTIES
+    IMPORTED_LOCATION "${YAJL_STATIC_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${YAJL_INCLUDE_DIR}"
+  )
 
-set(YAJL_LIBRARIES ${YAJL_LIBRARY} )
-set(YAJL_INCLUDE_DIRS ${YAJL_INCLUDE_DIR} )
+  set(YAJL_INCLUDE_DIRS ${YAJL_INCLUDE_DIR})
+  if (YAJL_USE_STATIC_LIBRARIES)
+    set(YAJL_LIBRARIES ${YAJL_STATIC_LIBRARY})
+  else ()
+    set(YAJL_LIBRARIES ${YAJL_SHARED_LIBRARY})
+  endif ()
+endif ()
+
+mark_as_advanced(YAJL_INCLUDE_DIR YAJL_SHARED_LIBRARY YAJL_STATIC_LIBRARY)
